529600b97d5ef1eb2da8056dd4714a19f84a471e
João Bahia <joaovictorbahia@gmail.com>
2023-09-29 10:14:22 +0100

    Push 1º

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp0
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Como se chama? ");
+			string Nomeproprio = Console.ReadLine();
+            Console.WriteLine("Bem vindo(a) {0}.", Nomeproprio);
+			
+		}
+	}
+}
