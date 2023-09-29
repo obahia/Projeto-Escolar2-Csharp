eaeee78d6bf9fa2bec8745990ff630a655adaadc
João Bahia <joaovictorbahia@gmail.com>
2023-09-29 11:08:48 +0100

    Att

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace ConsoleApp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, soma;
+            Console.WriteLine("Insira o primeiro numero: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira o segundo numero: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            soma =  n1 + n2;
+            Console.WriteLine("A soma do numero {0} e do numero {1} é {2}", n1, n2, soma);
+
+        }
+    }
+}
---
+++ Ficha de trab 1 Ex01/Ficha de trab 1 Ex01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+    namespace FT01
+{
+     internal class Programa01
+    {
+        static void Main (string[] args)
+        {
+            string nomeA, nomeE, nomeD;
+            Console.WriteLine("Insira seu nome e seu apelido: ");
+            nomeA = Console.ReadLine();
+            Console.WriteLine("\nInsira o nome da sua escola: ");
+            nomeE = Console.ReadLine();
+            Console.WriteLine("\nInsira o nome da sua disciplina: ");
+            nomeD = Console.ReadLine();
+            Console.WriteLine("\n\nNome do aluno: \n\n", nomeA);
+            Console.WriteLine("Nome da escola: \n\n", nomeE);
+            Console.WriteLine("Nome da disciplina: {2}\n\n", nomeD);
+
+        }
+    }
+}
